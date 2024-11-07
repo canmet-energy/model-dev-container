e32d54ee9ca50962671111fb9f41355a90886b91
plopez <phylroy.lopez@nrcan-rncan.gc.ca>
2024-11-06 23:05:46 -0500

    Update build_and_push.bat to include Docker push commands for version 3.8.0 and 3.7.0

--- build_and_push.bat
+++ build_and_push.bat
@@ -1,7 +1,7 @@
 REM Build Version 3.8.0
 docker build --build-arg OPENSTUDIO_VERSION=3.8.0 --build-arg OPENSTUDIO_HPXML_VERSION=1.7.0 --build-arg H2K_HPXML_VERSION=1.7.0.1.1 -t canmet/model_dev_container:3.8.0 .
+docker push canmet/model_dev_container:3.8.0 
 REM Build Version 3.7.0
 docker build --build-arg OPENSTUDIO_VERSION=3.7.0 --build-arg OPENSTUDIO_HPXML_VERSION=1.7.0 --build-arg H2K_HPXML_VERSION=1.7.0.1.1 -t canmet/model_dev_container:3.7.0 .
-REM Push to Docker Hub
-docker push canmet/model_dev_container:3.8.0 
 docker push canmet/model_dev_container:3.7.0 
+
