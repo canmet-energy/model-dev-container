5f1be78208d4a6b207ec78d5620fb7ecd6fcd053
plopez <phylroy.lopez@nrcan-rncan.gc.ca>
2024-11-14 16:51:28 -0500

    Update build_and_push.bat for version 3.9.0 and adjust dependencies for version 3.8.0

--- build_and_push.bat
+++ build_and_push.bat
@@ -1,5 +1,8 @@
+REM Build Version 3.9.0
+docker build --no-cache --build-arg OPENSTUDIO_VERSION=3.9.0 --build-arg OPENSTUDIO_HPXML_VERSION=1.8.1 --build-arg H2K_HPXML_VERSION=1.7.0.1.1 -t canmet/model_dev_container:3.9.0 .
+docker push canmet/model_dev_container:3.9.0 
 REM Build Version 3.8.0
-docker build --no-cache --build-arg OPENSTUDIO_VERSION=3.8.0 --build-arg OPENSTUDIO_HPXML_VERSION=1.7.0 --build-arg H2K_HPXML_VERSION=1.7.0.1.1 -t canmet/model_dev_container:3.8.0 .
+docker build --no-cache --build-arg OPENSTUDIO_VERSION=3.8.0 --build-arg OPENSTUDIO_HPXML_VERSION=1.8.1 --build-arg H2K_HPXML_VERSION=1.7.0.1.1 -t canmet/model_dev_container:3.8.0 .
 docker push canmet/model_dev_container:3.8.0 
 REM Build Version 3.7.0
 docker build --no-cache --build-arg OPENSTUDIO_VERSION=3.7.0 --build-arg OPENSTUDIO_HPXML_VERSION=1.7.0 --build-arg H2K_HPXML_VERSION=1.7.0.1.1 -t canmet/model_dev_container:3.7.0 .
